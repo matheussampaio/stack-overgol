a0d381d76f0d0e6e0b2bf1259043d66d75601e3a
Matheus Sampaio <matheus.sampaio@virtus.ufcg.edu.br>
2017-08-01 17:57:41 -0300

    chore: scripts to update ratings

--- src/ratings.py
+++ src/ratings.py
@@ -5,33 +5,55 @@ import pyrebase
 from operator import itemgetter
 
 STACK_OVERGOL_ID = '-1001071336991'
+RATINGS_FILE_NAME = 'ratings.csv'
+DEBUG = False
 
 db = pyrebase.initialize_app(configs.FIREBASE).database()
 
 def main():
-    with open('ratings2.csv', newline='') as ratingsfile:
+    with open(RATINGS_FILE_NAME, newline='') as ratingsfile:
         ratingsreader = csv.reader(ratingsfile, delimiter=',', quotechar='|')
 
         for i, row in enumerate(ratingsreader):
-            if i and row[4] != '-':
-                print(i, row)
+            if i and row[1] != '-' and row[2] != '-':
+                nome = row[0]
+                stars = float(row[1])
+                jogador_id = row[2]
 
-                jogador_id = row[4]
-                stars = float(row[2])
+                print("{}... ".format(nome), end="")
 
-                db.child("users/{}/groups/{}".format(jogador_id, STACK_OVERGOL_ID)).update({
-                    "stars": stars
-                })
+                path = "users/{}/groups/{}/stars".format(jogador_id, STACK_OVERGOL_ID)
 
-def get_current_list_and_ratings(group_id, show_id=False):
+                oldStars = db.child(path).get().val()
+
+                if (oldStars == stars):
+                    print("OK")
+                    continue
+
+                if DEBUG:
+                    print("(DEBUG) Atual: {} -> Novo: {}".format(oldStars, stars))
+                    continue
+
+                user_input = input("Atual: {} -> Novo: {} (s/n)? ".format(oldStars, stars))
+
+                if (user_input != "s"):
+                    print("Usuário não atualizado.")
+                    continue
+
+                db.child(path).set(stars)
+                print("Atualizado.")
+
+
+
+def get_missing_ratings_from_current_list(group_id, show_id=False):
     lista = db.child("groups/{}/lista".format(group_id)).get().val().values()
 
     lista = sorted(lista, key=itemgetter("first_name", "last_name"))
 
     for user in lista:
         id = user["id"]
-        first_name = user["first_name"]
-        last_name = user["last_name"]
+        first_name = user["first_name"].upper()
+        last_name = user["last_name"].upper()
 
         rating = db.child("users/{}/groups/{}/stars".format(id, group_id)).get().val()
 
@@ -41,9 +63,32 @@ def get_current_list_and_ratings(group_id, show_id=False):
             else:
                 output = ""
 
-            output += "{} {}".format(first_name, last_name)
+            output += ",{} {},,,{}".format(first_name, last_name, user["id"])
 
             print(output)
 
+
+# Firebase Admin: Search User
+# firebase().database().ref('/users').once('value', (snapshot) => {
+#     let result = '';
+#     const search = 'lindemberg'.toLowerCase();
+#
+#     const users = snapshot.val();
+#
+#     for (let key in users) {
+#         const user = users[key];
+#
+#         const nome = `${user.first_name} ${user.last_name}`;
+#
+#         if (nome.toLowerCase().indexOf(search) !== -1) {
+#             result += `${nome.toUpperCase()}\t\t\t${user.id}\n`;
+#         }
+#     }
+#
+#     console.log(result);
+# });
+
 if __name__ == "__main__":
-    get_current_list_and_ratings('-1001071336991')
+    get_missing_ratings_from_current_list(STACK_OVERGOL_ID)
+
+    # main()
