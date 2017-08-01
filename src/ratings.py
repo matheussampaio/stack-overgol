62fbd9003773d408b3b60f77b1910dcb3867ae0b
Matheus Sampaio <matheus.sampaio@virtus.ufcg.edu.br>
2017-08-01 14:36:01 -0300

    chore: search users without ratings

--- src/ratings.py
+++ src/ratings.py
@@ -2,11 +2,13 @@ import csv
 import configs
 import pyrebase
 
-group_id = '-1001071336991'
+from operator import itemgetter
+
+STACK_OVERGOL_ID = '-1001071336991'
+
+db = pyrebase.initialize_app(configs.FIREBASE).database()
 
 def main():
-    db = pyrebase.initialize_app(configs.FIREBASE).database()
-
     with open('ratings2.csv', newline='') as ratingsfile:
         ratingsreader = csv.reader(ratingsfile, delimiter=',', quotechar='|')
 
@@ -17,10 +19,31 @@ def main():
                 jogador_id = row[4]
                 stars = float(row[2])
 
-                db.child("users/{}/groups/{}".format(jogador_id, group_id)).update({
+                db.child("users/{}/groups/{}".format(jogador_id, STACK_OVERGOL_ID)).update({
                     "stars": stars
                 })
 
+def get_current_list_and_ratings(group_id, show_id=False):
+    lista = db.child("groups/{}/lista".format(group_id)).get().val().values()
+
+    lista = sorted(lista, key=itemgetter("first_name", "last_name"))
+
+    for user in lista:
+        id = user["id"]
+        first_name = user["first_name"]
+        last_name = user["last_name"]
+
+        rating = db.child("users/{}/groups/{}/stars".format(id, group_id)).get().val()
+
+        if (rating is None):
+            if show_id:
+                output = "{:10} - ".format(id)
+            else:
+                output = ""
+
+            output += "{} {}".format(first_name, last_name)
+
+            print(output)
 
 if __name__ == "__main__":
-    main()
+    get_current_list_and_ratings('-1001071336991')
